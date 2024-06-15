f08fa168080967341d08dc69fd56f863c502ecbd
Main Lucas <168214015+MainLucas@users.noreply.github.com>
2024-06-15 11:20:17 -0300

    Add files via upload

---
+++ IntruduçãoJava/maratonaDevDarian/src/javaCore/introducaometodos/dominio/Calculadora.java
@@ -0,0 +1,15 @@
+package javaCore.introducaometodos.dominio;
+
+public class Calculadora {
+
+     //métodos
+    public void somaDoisNumeros(){
+        System.out.println(10+10);
+    }
+    public void subtraiDoisNumeros(){
+        System.out.println(21-2);
+    }
+    public void multiplicaDoisNUmeros(int num1,int num2){
+        System.out.println(num1 * num2);
+    }
+}
---
+++ IntruduçãoJava/maratonaDevDarian/src/javaCore/introducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package javaCore.introducaometodos.test;
+
+import javaCore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNUmeros(2,4);
+    }
+}
