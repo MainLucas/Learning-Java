ed07d45f557442602701d0aac434d55eb78bf348
Main Lucas <168214015+MainLucas@users.noreply.github.com>
2024-06-22 14:44:56 -0300

    Add files via upload

---
+++ IntruduçãoJava/src/src/javaCore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package javaCore.Bintroducaometodos.test;
+
+import javaCore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1500,2000,3000});
+        funcionario.imprimi();
+
+        System.out.println("\nmédia: " + funcionario.getMedia());
+    }
+}
---
+++ IntruduçãoJava/src/src/javaCore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,15 @@
+package javaCore.Gassociacao.test;
+
+import javaCore.Gassociacao.dominio.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafu");
+        Jogador[] jogadores = new Jogador[]{jogador1,jogador2,jogador3};
+        for (Jogador jogador:jogadores){
+            jogador.imprime();
+        }
+    }
+}
